f046a42a1566ae2f40b5797ac5a3783958bbddf8
irsupera <97858090+irsupera@users.noreply.github.com>
2022-10-05 18:24:23 -0400

    Initial Commit
    Initial Commit

---
+++ app/src/main/java/com/isupera/recreationapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.isupera.recreationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
